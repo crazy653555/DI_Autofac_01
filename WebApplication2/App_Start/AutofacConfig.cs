ce8ccfd1704264992ab9fbcda6e87df94c55ff5d
eason <crazy653555@gmail.com>
2021-06-14 21:06:19 +0800

    autofac sample

---
+++ WebApplication2/App_Start/AutofacConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using WebApplication2.Controllers;
+
+namespace WebApplication2
+{
+    public class AutofacConfig
+    {
+        public static void Run()
+        {
+            // 1.建立容器
+            var builder = new ContainerBuilder();
+
+            // 2.註冊服務
+            //取得目前執行App的Assembly
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            // 註冊物件
+            builder.Register(c => new EMail()).As<IMessageService>();
+
+            // 註冊Controller和ApiController
+            builder.RegisterControllers(assembly); // Controller
+
+            // === 3. 由Builder建立容器 ===
+            var container = builder.Build();
+
+            // === 4. 把容器設定給DependencyResolver ===
+            // Controller
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMessageService _message;
+
+        public HomeController(IMessageService message)
+        {
+            _message = message;
+        }
+
+        public ActionResult Index()
+        {
+            var result = _message.Send("Hello DI");
+
+            ViewBag.Message = result;
+
+            return View();
+        }
+    }
+
+    public interface IMessageService
+    {
+        string Send(string message);
+    }
+
+    public class EMail : IMessageService
+    {
+        public string Send(string message)
+        {
+            return "Autofac-" + message;
+        }
+    }
+}
